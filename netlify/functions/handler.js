98577b03d542086bb1d95bdcb33dbb5c736d5083
GeForest <geniuss97it@gmail.com>
2024-03-11 19:41:37 +0300

    Change

--- netlify/functions/handler.js
+++ netlify/functions/handler.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-// const apiRoutes = require('../../backend/routes/apiRoutes');
 const apiRoutes = require('./apiRoutes');
 
 const { MONGODB_USERNAME, MONGODB_PASSWORD } = process.env;
@@ -11,7 +10,17 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.6h3lvf3.mongodb.net/pharmacyDB?retryWrites=true&w=majority&appName=Cluster0`);
+// Используйте асинхронный код для подключения к MongoDB
+const connectToMongoDB = async () => {
+  try {
+    await mongoose.connect(`mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@cluster0.6h3lvf3.mongodb.net/pharmacyDB?retryWrites=true&w=majority&appName=Cluster0`);
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+  }
+};
+
+connectToMongoDB();
 
 const connection = mongoose.connection;
 
@@ -30,7 +39,6 @@ mongoose.connection.on('error', (err) => {
 });
 
 app.use('/.netlify/functions/handler', apiRoutes);
-// app.use('/api', apiRoutes);
 
 module.exports = {
   handler: app,
