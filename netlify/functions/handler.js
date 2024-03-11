da7d057c5095d71ad6314b9aa46f7d9f9f308b47
GeForest <geniuss97it@gmail.com>
2024-03-12 00:25:54 +0300

    Change

--- netlify/functions/handler.js
+++ netlify/functions/handler.js
@@ -11,21 +11,13 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 
 
-mongoose.connect(`mongodb+srv://newUserReadWrite:HKQ6ez5l4zKBPLSM@cluster0.6h3lvf3.mongodb.net/pharmacyDB?retryWrites=true&w=majority&appName=Cluster0`);
+mongoose.connect(`mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@cluster0.6h3lvf3.mongodb.net/pharmacyDB?retryWrites=true&w=majority&appName=Cluster0`);
 
 const connection = mongoose.connection;
 
 const publicPath = path.join(__dirname, '..', 'frontend', 'build');
 app.use(express.static(publicPath));
 
-app.use('/api', apiRoutes);
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
-});
-
-connection.once('open', () => {
-  console.log('Connected to MongoDB');
-});
 connection.once('open', () => {
   console.log('Connected to MongoDB');
   app.listen(port, () => {
@@ -33,10 +25,21 @@ connection.once('open', () => {
   });
 });
 
+app.use('/api', apiRoutes);
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
 mongoose.connection.on('error', (err) => {
   console.error('MongoDB connection error:', err);
 });
 
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('MongoDB disconnected through app termination');
+    process.exit(0);
+  });
+});
 
 module.exports = {
   handler: app,
