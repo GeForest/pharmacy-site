4645d4c8b0aa2907eeb94a2357c3b15e5e81297b
GeForest <geniuss97it@gmail.com>
2024-03-11 03:24:00 +0300

    Change

--- netlify/functions/handler.js
+++ netlify/functions/handler.js
@@ -10,10 +10,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-mongoose.connect(`mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@ac-zlpldwr-shard-00-00.6h3lvf3.mongodb.net:27017,ac-zlpldwr-shard-00-01.6h3lvf3.mongodb.net:27017,ac-zlpldwr-shard-00-02.6h3lvf3.mongodb.net:27017/?replicaSet=atlas-luwyll-shard-0&ssl=true&authSource=admin`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(`mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@ac-zlpldwr-shard-00-00.6h3lvf3.mongodb.net:27017,ac-zlpldwr-shard-00-01.6h3lvf3.mongodb.net:27017,ac-zlpldwr-shard-00-02.6h3lvf3.mongodb.net:27017/?replicaSet=atlas-luwyll-shard-0&ssl=true&authSource=admin`);
 
 const connection = mongoose.connection;
 
