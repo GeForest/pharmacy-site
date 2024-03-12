7627a31e5c625175f812eee0d5e053603f1b6210
GeForest <geniuss97it@gmail.com>
2024-03-13 01:38:36 +0300

    Change

--- netlify/functions/handler.js
+++ netlify/functions/handler.js
@@ -70,7 +70,7 @@ app.use((req, res, next) => {
   res.status(500).json({ error: 'Function timed out' });
 });
 
-module.exports = app;
+module.exports = {handler: app};
 
 // async function initialize() {
 //   try {
