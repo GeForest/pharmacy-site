b6932488a6e736f27c11dd6ad15a9dac73331904
GeForest <geniuss97it@gmail.com>
2024-03-13 00:04:56 +0300

    Change

--- netlify/functions/handler.js
+++ netlify/functions/handler.js
@@ -12,10 +12,7 @@ app.use(express.json());
 
 async function handler(event, context) {
   try {
-    await mongoose.connect(`mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@cluster0.6h3lvf3.mongodb.net/pharmacyDB?retryWrites=true&w=majority&appName=Cluster0`, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect(`mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@cluster0.6h3lvf3.mongodb.net/pharmacyDB?retryWrites=true&w=majority&appName=Cluster0`);
 
     console.log('Connected to MongoDB');
 
