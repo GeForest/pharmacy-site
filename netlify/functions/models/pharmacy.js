5c99aad341738e14a94a5ee2a1f89154246ca70b
GeForest <geniuss97it@gmail.com>
2024-03-13 22:16:14 +0300

    Change

--- netlify/functions/models/pharmacy.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const pharmacySchema = new mongoose.Schema({
-  name: String,
-  location: String,
-  products_collection: String,
-});
-
-module.exports = mongoose.model('Pharmacy', pharmacySchema);
